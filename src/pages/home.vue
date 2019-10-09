ae964579b9de1dc96e2ecd05ef79b14ae27e4689
qiao22 <1481157584qq.com>
2019-10-09 15:11:29 +0800

    first  commit

---
+++ src/pages/home.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <p>{{$store.state.count}}</p>
+        <button @click="addHandler">+</button>
+        <button @click="eggHandle">-</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "home",
+        methods:{
+            addHandler(){
+                this.$store.commit('add')
+            },
+            eggHandle(){
+                this.$store.commit('egg')
+
+            }
+        }
+    }
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
