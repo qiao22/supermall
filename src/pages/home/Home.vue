445198918df3cf3a02ba176f6b5e61877cdf4ba9
qiao22 <1481157584qq.com>
2019-10-29 23:19:17 +0800

    添加初始化样式

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        home
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: "Home",
+
+    }
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
